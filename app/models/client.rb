4b7cc7d57e3f13221ae7d83a89259a0590ca96d4
Peter Bell <peter.bell215@gmail.com>
2022-12-30 05:26:55 +0000

    Add dependent destroy for Client child models

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+# Provides details of a client including address.
 class Client < ApplicationRecord
+  has_many :sessions, dependent: :destroy
+  has_many :session_charges, dependent: :destroy
 end
